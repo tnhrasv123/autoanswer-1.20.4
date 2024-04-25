ed2e0b3c607ad234f942c8ce0ad2fe695cfaf734
tnhrasv123 <virusgamingandrecording@gmail.com>
2024-04-25 08:38:47 +0300

    Very Important change

---
+++ src/main/java/net/amir/autoanswer/AutoAnswerClient.java
@@ -0,0 +1,10 @@
+package net.amir.autoanswer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoAnswerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
